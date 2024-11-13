76f6c31962fcd1cd710bdb96efdbf83955eb232e
amiradamov <adamov.aamir@gmail.com>
2024-11-13 20:08:49 +0400

    some changes

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-from dotenv import load_dotenv
 import os
 
-# Load environment variables from the .env file
-load_dotenv()
-
+# Access environment variables directly (no need for dotenv on Vercel)
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
