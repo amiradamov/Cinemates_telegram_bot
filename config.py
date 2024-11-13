2ce1850040bc9b864f0beb26d9a6b70120cc525c
amiradamov <adamov.aamir@gmail.com>
2024-11-13 18:16:25 +0400

    some changes

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-# config.py
-TMDB_API_KEY = 'cee873f2f8206522d9688056b6009b54'
-TELEGRAM_BOT_TOKEN = '6888713348:AAESwQVa6nF3eai5T-vr30d1w2urquVa60o'
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
